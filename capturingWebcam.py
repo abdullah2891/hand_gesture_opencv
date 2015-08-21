21f0ac17eb40b4f2614812564a448113e23d6171
abdullah2891 <abdullah2891@gmail.com>
2015-08-21 10:06:58 +0600

    applying harris corner detection

--- capturingWebcam.py
+++ capturingWebcam.py
@@ -6,6 +6,8 @@ __author__ = 'Abdullah_Rahman'
 
 cap = cv2.VideoCapture(0)
 
+
+
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
@@ -13,8 +15,17 @@ while(True):
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
+
+    gray=np.float32(gray)
+    dst=cv2.cornerHarris(gray,2,3,0.04)
+    dst=cv2.dilate(dst,None)
+    frame[dst>0.001*dst.max()]=[0,0,255]
+    print dst
+
+
     # Display the resulting frame
-    cv2.imshow('frame',gray)
+
+    cv2.imshow('frame',frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
