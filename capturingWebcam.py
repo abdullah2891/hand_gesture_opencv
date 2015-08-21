18b0709cc5afa6b5007a192d60a8befee57b8d1e
abdullah2891 <abdullah2891@gmail.com>
2015-08-22 02:25:08 +0600

    orb descriptor applied on webcam have to adjust

--- capturingWebcam.py
+++ capturingWebcam.py
@@ -6,26 +6,34 @@ __author__ = 'Abdullah_Rahman'
 
 cap = cv2.VideoCapture(0)
 
-trainer=cv2.imread('box-03.jpg',0)
+trainer=cv2.imread('hand6.jpg',0)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = cap.read()
 
     # Our operations on the frame come here
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
 
-    gray=np.float32(gray)
-    dst=cv2.cornerHarris(gray,2,3,0.04)
-    dst=cv2.dilate(dst,None)
-    frame[dst>0.01*dst.max()]=[0,0,255]
-    print dst
+    orb=cv2.ORB_create()
 
+    kp1, des1=orb.detectAndCompute(frame,None)
+    kp2, des2=orb.detectAndCompute(trainer,None)
+
+    # create BFMatcher object
+    bf = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=True)
+
+    # Match descriptors.
+    matches = bf.match(des1, des2)
+
+    # Sort them in the order of their distance.
+    matches = sorted(matches, key = lambda x:x.distance)
+
+    img3 = cv2.drawMatches(frame,kp1,trainer,kp2,matches[:5],outImg=None)
 
     # Display the resulting frame
 
-    cv2.imshow('frame',frame)
+    cv2.imshow('frame',img3)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
