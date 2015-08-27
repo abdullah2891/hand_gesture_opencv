4f60d0373c935b44b9158fc977ec4148285950c9
abdullah2891 <abdullah2891@gmail.com>
2015-08-27 23:14:20 +0600

    -hand detection improved  thresholding
    -controlling mouse
         *perform calibration
         *stablize

--- hand_cascade.py
+++ hand_cascade.py
@@ -1,4 +1,4 @@
-#your mouse will go banana don't use it yet
+#your mouse will go banana don't run it yet
 
 import cv2
 import pyautogui as mouse
@@ -9,28 +9,34 @@ __author__ = 'Abdullah_Rahman'
 hand = cv2.CascadeClassifier('Hand.Cascade.1.xml')
 cap=cv2.VideoCapture(0)
 
-
+screen=mouse.size()
 
 while(1):
     ret, frame= cap.read()
 
+
     gray=cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
 
+    ret,thresholded=cv2.threshold(frame,100,255,cv2.THRESH_TOZERO)
     pos_hand=hand.detectMultiScale(
-        frame,
-        scaleFactor=1.5,
-        minNeighbors=6,
-        minSize=(40, 40)
+        thresholded,
+        scaleFactor=1.2,
+        minNeighbors=3,
+        minSize=(50, 50)
     )
 
 
+    sum_x=0
+    sum_y=0
 
 
     for (x,y,w,h) in pos_hand:
-        mouse.moveTo(x,y)
+
+        mouse.moveTo((screen[0]-x),(screen[1]-y))
         cv2.rectangle(frame, (x,y),(x+w, y+h), (255, 0, 0), 2)
-        roi_gray = gray[y:y+h, x:x+w]
+        roi_gray = frame[y:y+h, x:x+w]
         roi_color = frame[y:y+h, x:x+w]
+
 
 
 
