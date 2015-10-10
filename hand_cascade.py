8062381bb69d7e9fcf05e3ef645cf1af381b3a81
abdullah2891 <abdullah2891@gmail.com>
2015-10-11 01:27:52 +0600

    Added mouse movement(smoother than before) Need to Calibrate with monitor

--- hand_cascade.py
+++ hand_cascade.py
@@ -54,6 +54,7 @@ while(1):
 
     if(len(X)>10):
         cv2.rectangle(frame, (smoother(X), smoother(Y)), (smoother(X)+smoother(W),smoother(Y)+smoother(H)), (0, 255, 0), 2)
+        mouse.moveTo(smoother(X),smoother(Y),0.5)
         X=[];Y=[];W=[];H=[]
 
 
